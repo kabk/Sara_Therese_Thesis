37c9b4db847e2fec606ab4232f84c503b14dd739
SaraTherese <bruker@Brukers-MacBook-Pro-3.local>
2015-01-24 19:10:10 +0100

    Complete Website
    From local to web

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+
+
+	$('.theme').click(function() {
+		var value = $(this).attr("id");
+		var file = "css/"+value+".css";
+
+		// clear the selected theme and mark one as selected
+		 $('.theme').each(function () {
+			 $(this).removeClass('selected');
+		 });
+		$(this).addClass('selected');
+
+		// apply the theme
+		$("#theme").attr("href", file);
+
+		return false;
+	});
+
+
+	$(".prefs-link").pageslide({ modal: true });
+	$(".prefs-link1").pageslide({ modal: true });
+
+
+});
